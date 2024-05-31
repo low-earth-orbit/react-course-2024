5112e234f06e72cd413f125a36ba5af9aed58dba
Leo Hong <5917188+low-earth-orbit@users.noreply.github.com>
2024-05-30 21:23:47 -0300

    section 31 up to styling

---
+++ 31-react-summary/src/App.jsx
@@ -0,0 +1,10 @@
+import Post from "./components/Post";
+
+function App() {
+  return <main>
+    <Post author="Max" body="React is awesome!"/>
+    <Post author="Manual" body="Check out the course!"/>
+    </main>;
+}
+
+export default App;
