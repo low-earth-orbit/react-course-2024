177226d5aabacc97464d5526957b6ef007ae04a5
Leo Hong <5917188+low-earth-orbit@users.noreply.github.com>
2024-06-01 20:27:59 -0300

    588 state lifting

--- 31-react-summary/src/components/PostList.jsx
+++ 31-react-summary/src/components/PostList.jsx
@@ -1,13 +1,28 @@
+import { useState } from "react";
 import Post from "./Post";
 import classes from "./PostList.module.css";
 import NewPost from "./NewPost";
 
 function PostList(props) {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function changeBodyHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function onAuthorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
   return (
     <>
-      <NewPost />
+      <NewPost
+        onBodyChange={changeBodyHandler}
+        onAuthorChange={onAuthorChangeHandler}
+      />
       <ul className={classes.posts}>
-        <Post author="Max" body="React is awesome!" />
+        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Manual" body="Check out the course!" />
       </ul>
     </>
