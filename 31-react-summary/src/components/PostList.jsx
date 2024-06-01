0331c809a1a4b98fad08e104c7512b31ee187087
Leo Hong <5917188+low-earth-orbit@users.noreply.github.com>
2024-06-01 00:46:57 -0300

    Finish 587

--- 31-react-summary/src/components/PostList.jsx
+++ 31-react-summary/src/components/PostList.jsx
@@ -1,12 +1,16 @@
 import Post from "./Post";
 import classes from "./PostList.module.css";
+import NewPost from "./NewPost";
 
 function PostList(props) {
   return (
-    <ul className={classes.posts}>
-      <Post author="Max" body="React is awesome!" />
-      <Post author="Manual" body="Check out the course!" />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post author="Max" body="React is awesome!" />
+        <Post author="Manual" body="Check out the course!" />
+      </ul>
+    </>
   );
 }
 
