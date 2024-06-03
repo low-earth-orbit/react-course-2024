45ce48d359e9f6ca9e0a9fab50d63147baf7c61b
Leo Hong <5917188+low-earth-orbit@users.noreply.github.com>
2024-06-02 23:01:17 -0300

    Finish 595

--- 31-react-summary/src/components/PostList.jsx
+++ 31-react-summary/src/components/PostList.jsx
@@ -19,11 +19,18 @@ function PostList({ isPosting, onStopPosting }) {
         </Modal>
       )}
 
-      <ul className={classes.posts}>
-        {posts.map((post) => (
-          <Post key={post.body} author={post.author} body={post.body} />
-        ))}
-      </ul>
+      {posts.length > 0 ? (
+        <ul className={classes.posts}>
+          {posts.map((post) => (
+            <Post key={post.body} author={post.author} body={post.body} />
+          ))}
+        </ul>
+      ) : (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>There are no posts yet.</h2>
+          <p>Start adding some!</p>
+        </div>
+      )}
     </>
   );
 }
