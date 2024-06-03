1628a8df49bd381234ffcf124070c29e0267dad1
Leo Hong <5917188+low-earth-orbit@users.noreply.github.com>
2024-06-02 22:42:40 -0300

    593

--- 31-react-summary/src/components/PostList.jsx
+++ 31-react-summary/src/components/PostList.jsx
@@ -1,35 +1,22 @@
-import { useState } from "react";
 import Post from "./Post";
 import classes from "./PostList.module.css";
 import NewPost from "./NewPost";
 import Modal from "./Modal";
 
 function PostList({ isPosting, onStopPosting }) {
-  const [enteredBody, setEnteredBody] = useState("");
-  const [enteredAuthor, setEnteredAuthor] = useState("");
-
-  function changeBodyHandler(event) {
-    setEnteredBody(event.target.value);
-  }
-
-  function onAuthorChangeHandler(event) {
-    setEnteredAuthor(event.target.value);
-  }
-
   return (
     <>
       {isPosting && (
         <Modal onClose={onStopPosting}>
           <NewPost
-            onBodyChange={changeBodyHandler}
-            onAuthorChange={onAuthorChangeHandler}
+            // onBodyChange={changeBodyHandler}
+            // onAuthorChange={onAuthorChangeHandler}
             onCancel={onStopPosting}
           />
         </Modal>
       )}
 
       <ul className={classes.posts}>
-        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Manual" body="Check out the course!" />
       </ul>
     </>
