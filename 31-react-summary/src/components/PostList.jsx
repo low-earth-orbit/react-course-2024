9881db3deb9349bce631bb0d4c12770d511d00b0
Leo Hong <5917188+low-earth-orbit@users.noreply.github.com>
2024-06-04 23:21:41 -0300

    603

--- 31-react-summary/src/components/PostList.jsx
+++ 31-react-summary/src/components/PostList.jsx
@@ -2,10 +2,8 @@ import { useState, useEffect } from "react";
 
 import Post from "./Post";
 import classes from "./PostList.module.css";
-import NewPost from "./NewPost";
-import Modal from "./Modal";
 
-function PostList({ isPosting, onStopPosting }) {
+function PostList() {
   const [posts, setPosts] = useState([]);
 
   const [isFetching, setIsFetching] = useState(true);
@@ -34,12 +32,6 @@ function PostList({ isPosting, onStopPosting }) {
 
   return (
     <>
-      {isPosting && (
-        <Modal onClose={onStopPosting}>
-          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
-        </Modal>
-      )}
-
       {!isFetching && posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post) => (
