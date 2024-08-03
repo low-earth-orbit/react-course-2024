c6707b81ab692c4c0506b2159e35fb3795b7bc21
Leo Hong <5917188+low-earth-orbit@users.noreply.github.com>
2024-08-03 01:56:38 -0300

    66

---
+++ 04-react-deep-dive/src/App.jsx
@@ -0,0 +1,20 @@
+import Header from "./components/Header/Header.jsx";
+import CoreConcepts from "./components/CoreConcepts.jsx";
+import Examples from "./components/Examples.jsx";
+
+function App() {
+
+  console.log("APP COMPONENT EXECUTING");
+
+  return (
+    <>
+      <Header />
+      <main>
+        <CoreConcepts />
+        <Examples />
+      </main>
+    </>
+  );
+}
+
+export default App;
