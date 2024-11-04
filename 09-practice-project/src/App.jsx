c862569f9a54580f06af6bebadf603e89661db16
Leo Hong <5917188+low-earth-orbit@users.noreply.github.com>
2024-11-03 23:38:18 -0400

    149

--- 09-practice-project/src/App.jsx
+++ 09-practice-project/src/App.jsx
@@ -1,8 +1,10 @@
+import SidePanel from "./components/SidePanel";
+
 function App() {
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8">
+      <SidePanel />
+    </main>
   );
 }
 
