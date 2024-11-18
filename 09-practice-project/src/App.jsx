7c9278a4c2386c9948f695dcb2dc46fda9e6b8ed
Leo Hong <5917188+low-earth-orbit@users.noreply.github.com>
2024-11-17 23:41:26 -0400

    151

--- 09-practice-project/src/App.jsx
+++ 09-practice-project/src/App.jsx
@@ -1,9 +1,11 @@
+import NewProject from "./components/NewProject";
 import SidePanel from "./components/SidePanel";
 
 function App() {
   return (
-    <main className="h-screen my-8">
+    <main className="h-screen my-8 flex gap-8">
       <SidePanel />
+      <NewProject />
     </main>
   );
 }
