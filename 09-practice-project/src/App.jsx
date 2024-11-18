0b5e483ed91c5aa7f23cb455ec343c39ffbe5d2a
Leo Hong <5917188+low-earth-orbit@users.noreply.github.com>
2024-11-17 23:51:00 -0400

    152

--- 09-practice-project/src/App.jsx
+++ 09-practice-project/src/App.jsx
@@ -1,4 +1,5 @@
 import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 import SidePanel from "./components/SidePanel";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <main className="h-screen my-8 flex gap-8">
       <SidePanel />
       <NewProject />
+      <NoProjectSelected />
     </main>
   );
 }
