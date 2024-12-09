d74645538bb7b7c9169e90db5bd3613593a553aa
Leo Hong <5917188+low-earth-orbit@users.noreply.github.com>
2024-12-08 22:56:47 -0400

    157

--- 09-practice-project/src/App.jsx
+++ 09-practice-project/src/App.jsx
@@ -33,12 +33,21 @@ function App() {
     });
   }
 
+  function handleCancelAddProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+      };
+    });
+  }
+
   console.log(projectsState);
 
   let content;
 
   if (projectsState.selectedProjectId === null) {
-    content = <NewProject onAdd={handleAddProject} />;
+    content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />;
   } else if (projectsState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
   }
