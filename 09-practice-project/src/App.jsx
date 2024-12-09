b7b89abdd79c6c320e7a9ef5b07ca9e6433790bb
Leo Hong <5917188+low-earth-orbit@users.noreply.github.com>
2024-12-08 20:45:19 -0400

    153

--- 09-practice-project/src/App.jsx
+++ 09-practice-project/src/App.jsx
@@ -1,13 +1,35 @@
-import NewProject from "./components/NewProject";
+import { useState } from "react";
 import NoProjectSelected from "./components/NoProjectSelected";
 import SidePanel from "./components/SidePanel";
+import NewProject from "./components/NewProject";
 
 function App() {
+  const [projectsState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <SidePanel />
-      <NewProject />
-      <NoProjectSelected />
+      <SidePanel onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
