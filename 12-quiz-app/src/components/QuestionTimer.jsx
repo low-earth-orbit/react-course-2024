0cdb4e3adb36555fcfcfbb914d756ea9c3a5570b
Leo Hong <5917188+low-earth-orbit@users.noreply.github.com>
2025-01-15 02:02:23 -0400

    199

---
+++ 12-quiz-app/src/components/QuestionTimer.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+
+export default function QuestionTimer({ timeout, onTimeout }) {
+  const [remainingTime, setRemainingTime] = useState(timeout);
+
+  // this useEffect is for calling onTimeout function to update userAnswers with null answer for the current question
+  // after timer expires.
+  useEffect(() => {
+    const timer = setTimeout(
+      // time is out. we should move on to the next question
+      onTimeout,
+      timeout
+    );
+
+    // clear the timer
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [timeout, onTimeout]); // onTimeout is memoized using useCallback in Quiz component. It will not update every time QuestionTimer is rerendered
+
+  // This useEffect is for displaying the progress bar.
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRemainingTime((prev) => prev - 100);
+    }, 100);
+
+    // clear up existing interval to ensure only one interval running at the same time
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return <progress id="question-time" max={timeout} value={remainingTime} />;
+}
--- 12-quiz-app/src/components/Quiz.jsx
+++ 12-quiz-app/src/components/Quiz.jsx
@@ -1,11 +1,19 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import QUESTIONS from "../questions.js";
 import quizCompleting from "../assets/quiz-complete.png";
+import QuestionTimer from "./QuestionTimer.jsx";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]); // array of user answers
+  console.log("test userAnswers =", userAnswers);
   const activeQuestionIndex = userAnswers.length; // derived state
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
+  const handleSelectAnswer = useCallback(function handleSelectAnswer(answer) {
+    setUserAnswers((prevAnswers) => [...prevAnswers, answer]);
+  }, []);
+
+  const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), []);
 
   if (quizIsComplete) {
     return (
@@ -19,14 +27,17 @@ export default function Quiz() {
   const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers]; // this creates a new array
   shuffledAnswers.sort(() => Math.random() - 0.5); // this changes shuffledAnswers
 
-  function handleSelectAnswer(answer) {
-    setUserAnswers((prevAnswers) => [...prevAnswers, answer]);
-  }
-
   return (
     <div id="quiz">
       <div id="question">
-        <h2> {QUESTIONS[activeQuestionIndex].text}</h2>
+        {/* key changes react will unmount and remount the component */}
+        {/* without adding key prop here, the component will not rerender because timeout and onTimeout values do not change */}
+        <QuestionTimer
+          key={activeQuestionIndex}
+          timeout={10000}
+          onTimeout={handleSkipAnswer}
+        />
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
           {shuffledAnswers.map((answer) => (
             <li key={answer} className="answer">
