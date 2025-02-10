ecf422991f94c1dee0c69583c715deaa4e28ee6c
Leo Hong <5917188+low-earth-orbit@users.noreply.github.com>
2025-02-09 22:36:16 -0400

    237: error message

--- 15-http/src/components/AvailablePlaces.jsx
+++ 15-http/src/components/AvailablePlaces.jsx
@@ -1,26 +1,41 @@
 import Places from "./Places.jsx";
 import { useState, useEffect } from "react";
+import ErrorPage from "./Error.jsx";
 
 const places = localStorage.getItem("places");
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
   const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState();
 
   useEffect(() => {
     async function fetchPlaces() {
       setIsFetching(true);
 
-      const response = await fetch("http://localhost:3000/places");
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
+      try {
+        const response = await fetch("http://localhost:3000/places"); // error could occur here
+        const resData = await response.json();
 
-      setIsFetching(false);
+        if (!response.ok) {
+          throw new Error("Failed to fetch places"); // this is what we consider an error response (if not 200, 300)
+        }
+
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+        setError({
+          message: error.message || "Could not fetch places, please try again",
+        });
+      }
+
+      setIsFetching(false); // end loading state not matter it succeeds or not
     }
 
     fetchPlaces();
   }, []);
 
+  if (error)
+    return <ErrorPage title="An error occurred" message={error.message} />;
   return (
     <Places
       title="Available Places"
