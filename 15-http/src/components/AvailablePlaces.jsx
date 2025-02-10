e7663aefa4bf1fe40c48719b2db2150dbf648691
Leo Hong <5917188+low-earth-orbit@users.noreply.github.com>
2025-02-09 22:21:03 -0400

    235: loading states

--- 15-http/src/components/AvailablePlaces.jsx
+++ 15-http/src/components/AvailablePlaces.jsx
@@ -5,12 +5,17 @@ const places = localStorage.getItem("places");
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
 
   useEffect(() => {
     async function fetchPlaces() {
+      setIsFetching(true);
+
       const response = await fetch("http://localhost:3000/places");
       const resData = await response.json();
       setAvailablePlaces(resData.places);
+
+      setIsFetching(false);
     }
 
     fetchPlaces();
@@ -20,6 +25,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
