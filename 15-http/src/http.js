6fc8f031c4a21314ce16643dd2fa30f64359b766
Leo Hong <5917188+low-earth-orbit@users.noreply.github.com>
2025-03-09 23:30:40 -0300

    240: PUT request

--- 15-http/src/http.js
+++ 15-http/src/http.js
@@ -1,9 +1,27 @@
 export async function fetchAvailablePlaces() {
-  const response = await fetch("http://localhost:3000/places");
+  const response = await fetch("http://localhost:3000/places"); // default is get method
   const resData = await response.json();
 
   if (!response.ok) {
     throw new Error("Failed to fetch places");
   }
   return resData.places;
+}
+
+export async function updateUserPlaces(places) {
+  const response = await fetch("http://localhost:3000/user-places", {
+    method: "PUT",
+    body: JSON.stringify({ places }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const resData = await response.json();
+
+  if (!response.ok) {
+    throw new Error("Failed to update user data.");
+  }
+
+  return resData.message;
 }
