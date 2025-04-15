32a04f9e53c55092a53ac4fb4bf37871982edbad
Leo Hong <5917188+low-earth-orbit@users.noreply.github.com>
2025-04-14 23:26:40 -0300

    242: fetch user places

--- 15-http/src/http.js
+++ 15-http/src/http.js
@@ -1,9 +1,19 @@
 export async function fetchAvailablePlaces() {
-  const response = await fetch("http://localhost:3000/places"); // default is get method
+  const response = await fetch("http://localhost:3000/places"); // default is GET method
   const resData = await response.json();
 
   if (!response.ok) {
     throw new Error("Failed to fetch places");
+  }
+  return resData.places;
+}
+
+export async function fetchUserPlaces() {
+  const response = await fetch("http://localhost:3000/user-places");
+  const resData = await response.json();
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch user places");
   }
   return resData.places;
 }
