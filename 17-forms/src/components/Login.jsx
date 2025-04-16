a1165a5b8e06b9914f6fea4831376d98a0344ad6
Leo Hong <5917188+low-earth-orbit@users.noreply.github.com>
2025-04-16 00:17:41 -0300

    254: multiple input fields

--- 17-forms/src/components/Login.jsx
+++ 17-forms/src/components/Login.jsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
+
 export default function Login() {
+  const [enteredValues, setEnteredValues] = useState({
+    email: "",
+    password: "",
+  });
+
   function handleSubmit(event) {
     event.preventDefault();
     console.log("Form submitted");
+    console.log("enteredValues:", enteredValues);
+  }
+
+  function handleInputChange(identifier, event) {
+    setEnteredValues((prevValues) => ({
+      ...prevValues,
+      [identifier]: event.target.value,
+    }));
   }
 
   return (
@@ -11,12 +26,24 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={(event) => handleInputChange("email", event)}
+            value={enteredValues.email}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={(event) => handleInputChange("password", event)}
+            value={enteredValues.password}
+          />
         </div>
       </div>
 
