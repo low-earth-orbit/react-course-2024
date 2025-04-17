9d547cc85d3ca8f96d5270aebcff1d02e6727c27
Leo Hong <5917188+low-earth-orbit@users.noreply.github.com>
2025-04-17 01:30:51 -0300

    265: custom useInput hook

--- 17-forms/src/components/StateLogin.jsx
+++ 17-forms/src/components/StateLogin.jsx
@@ -1,50 +1,26 @@
-import { useState } from "react";
 import Input from "./Input";
 import { hasMinLength, isEmail, isNotEmpty } from "../util/validation";
+import { useInput } from "../hooks/useInput";
 
 export default function Login() {
-  const [enteredValues, setEnteredValues] = useState({
-    email: "",
-    password: "",
-  });
+  const {
+    value: emailValue,
+    handleInputChange: handleEmailChange,
+    handleInputBlur: handleEmailBlur,
+    hasError: emailHasError,
+  } = useInput("", (value) => isEmail(value) && isNotEmpty(value));
 
-  const [didEdit, setDidEdit] = useState({
-    email: false,
-    password: false,
-  });
-
-  const emailIsInvalid =
-    didEdit.email &&
-    !isEmail(enteredValues.email) &&
-    isNotEmpty(enteredValues.email);
-
-  const passwordIsInvalid =
-    didEdit.password &&
-    !hasMinLength(enteredValues.password, 6) &&
-    isNotEmpty(enteredValues.password);
+  const {
+    value: passwordValue,
+    handleInputChange: handlePasswordChange,
+    handleInputBlur: handlePasswordBlur,
+    hasError: passwordHasError,
+  } = useInput("", (value) => hasMinLength(value, 6) && isNotEmpty(value));
 
   function handleSubmit(event) {
     event.preventDefault();
     console.log("Form submitted");
     console.log("enteredValues:", enteredValues);
-  }
-
-  function handleInputChange(identifier, event) {
-    setEnteredValues((prevValues) => ({
-      ...prevValues,
-      [identifier]: event.target.value,
-    }));
-    setDidEdit((prevValues) => ({
-      ...prevValues,
-      [identifier]: false, // Reset the edited state when the user types in the input
-    }));
-  }
-
-  function handleInputBlur(identifier) {
-    setDidEdit((prevValues) => ({
-      ...prevValues,
-      [identifier]: true, // Mark the field as edited when the input loses focus
-    }));
   }
 
   return (
@@ -57,10 +33,10 @@ export default function Login() {
           label="Email"
           type="email"
           name="email"
-          onBlur={() => handleInputBlur("email")}
-          onChange={(event) => handleInputChange("email", event)}
-          value={enteredValues.email}
-          error={emailIsInvalid && "Please enter a valid email address."}
+          onBlur={handleEmailBlur}
+          onChange={handleEmailChange}
+          value={emailValue}
+          error={emailHasError && "Please enter a valid email!"}
         />
 
         <Input
@@ -68,12 +44,10 @@ export default function Login() {
           id="password"
           type="password"
           name="password"
-          onBlur={() => handleInputBlur("password")}
-          onChange={(event) => handleInputChange("password", event)}
-          value={enteredValues.password}
-          error={
-            passwordIsInvalid && "Password must be between 6 and 20 characters."
-          }
+          onBlur={handlePasswordBlur}
+          onChange={handlePasswordChange}
+          value={passwordValue}
+          error={passwordHasError && "Password length must be greater than 6!"}
         />
       </div>
 
