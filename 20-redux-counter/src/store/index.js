4fc10b7d7ddf2c0b2457b121bd1c1148e0d2f148
Leo Hong <5917188+low-earth-orbit@users.noreply.github.com>
2025-04-27 15:52:51 -0300

    323: split slices

--- 20-redux-counter/src/store/index.js
+++ 20-redux-counter/src/store/index.js
@@ -1,57 +1,12 @@
-import { createSlice, configureStore } from "@reduxjs/toolkit";
-
-const initialCounterState = {
-  value: 0,
-  showCounter: true,
-};
-
-const counterSlice = createSlice({
-  name: "counter",
-  initialState: initialCounterState,
-  reducers: {
-    increment(state) {
-      state.value++;
-    },
-    decrement(state) {
-      state.value--;
-    },
-    increase(state, action) {
-      state.value = state.value + action.payload;
-    },
-    decrease(state, action) {
-      state.value = state.value - action.payload;
-    },
-    toggleCounter(state) {
-      state.showCounter = !state.showCounter;
-    },
-  },
-});
-
-const initialAuthState = {
-  loggedIn: false,
-};
-
-const authSlice = createSlice({
-  name: "auth",
-  initialState: initialAuthState,
-  reducers: {
-    login(state) {
-      state.loggedIn = true;
-    },
-    logout(state) {
-      state.loggedIn = false;
-    },
-  },
-});
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./counter";
+import authReducer from "./auth";
 
 const store = configureStore({
   reducer: {
-    counter: counterSlice.reducer,
-    auth: authSlice.reducer,
+    counter: counterReducer,
+    auth: authReducer,
   },
 });
-
-export const counterActions = counterSlice.actions;
-export const authActions = authSlice.actions;
 
 export default store;
