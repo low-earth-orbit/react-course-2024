28d0c5a56d9670fcc7382b27fd45be1c73f9a6a1
Leo Hong <5917188+low-earth-orbit@users.noreply.github.com>
2025-04-26 22:55:32 -0300

    296: custom hook for fetching meals data

--- 19-food-order/src/components/Meals.jsx
+++ 19-food-order/src/components/Meals.jsx
@@ -1,39 +1,25 @@
-import { useState, useEffect } from "react";
 import MealItem from "./MealItem";
+import useHttp from "../hooks/useHttp";
+
+const requestConfig = {};
+const initialData = [];
 
 export default function Meals() {
-  const [loadedMeals, setLoadedMeals] = useState([]);
+  const {
+    data: loadedMeals,
+    isLoading,
+    error,
+  } = useHttp("http://localhost:3000/meals", requestConfig, initialData);
 
-  useEffect(() => {
-    async function fetchMeals() {
-      let response;
-      try {
-        response = await fetch("http://localhost:3000/meals");
-      } catch (error) {
-        console.error("Error fetching meals:", error);
-      } // this try catch block catches errors in the fetch call
-      // but not errors in the response
-
-      if (!response.ok) {
-        console.error("Failed to fetch meals");
-        return;
-      } // this if statement checks if the response is ok
-
-      const resData = await response.json();
-
-      setLoadedMeals(resData); // this line sets the loadedMeals state to the response data
-    }
-
-    fetchMeals();
-  }, []);
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <ul id="meals">
-      {loadedMeals.length > 0 ? (
-        loadedMeals.map((meal) => <MealItem key={meal.id} meal={meal} />)
-      ) : (
-        <p>No meals available</p>
-      )}
+      {loadedMeals.map((meal) => (
+        <MealItem key={meal.id} meal={meal} />
+      ))}
     </ul>
   );
 }
---
+++ 19-food-order/src/hooks/useHttp.js
@@ -0,0 +1,48 @@
+import { useEffect, useState, useCallback } from "react";
+
+async function sendHttpRequest(url, config) {
+  const response = await fetch(url, config);
+
+  const resData = await response.json();
+
+  if (!response.ok) {
+    throw new Error(resData.message || "Failed to send request.");
+  }
+
+  return resData;
+}
+
+export default function useHttp(url, config, initialData) {
+  const [data, setData] = useState(initialData);
+  const [error, setError] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const sendRequest = useCallback(
+    async function sendRequest() {
+      setIsLoading(true);
+
+      try {
+        const resData = await sendHttpRequest(url, config);
+        setData(resData);
+      } catch (error) {
+        setError(error.message || "Something went wrong!");
+      }
+
+      setIsLoading(false);
+    },
+    [url, config]
+  );
+
+  useEffect(() => {
+    if (!config || (config && (config.method === "GET" || !config.method))) {
+      sendRequest();
+    }
+  }, [sendRequest]);
+
+  return {
+    data,
+    error,
+    isLoading,
+    sendRequest,
+  };
+}
