c7705b2b149d0b357d5c9ef481ae1a005ed45ed7
Leo Hong <5917188+low-earth-orbit@users.noreply.github.com>
2025-05-10 19:17:19 -0300

    389 auth action

--- 23-authentication/frontend/src/components/AuthForm.js
+++ 23-authentication/frontend/src/components/AuthForm.js
@@ -16,7 +16,13 @@ function AuthForm() {
         </p>
         <p>
           <label htmlFor="image">Password</label>
-          <input id="password" type="password" name="password" required />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            autoComplete="current-password"
+            required
+          />
         </p>
         <div className={classes.actions}>
           <Link to={`?mode=${isLogin ? "signup" : "login"}`}>
