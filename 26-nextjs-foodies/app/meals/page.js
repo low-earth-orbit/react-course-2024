489e730ad1b12b4d08474722f7515a193eb237ce
Leo Hong <5917188+low-earth-orbit@users.noreply.github.com>
2025-05-18 18:51:16 -0300

    448 add Meals page content

--- 26-nextjs-foodies/app/meals/page.js
+++ 26-nextjs-foodies/app/meals/page.js
@@ -1,8 +1,23 @@
+import MealsGrid from "@/components/meals/meals-grid";
+import classes from "./page.module.css";
+import Link from "next/link";
+
 export default function MealsPage() {
   return (
-    <main>
-      <h1>Meals</h1>
-      
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>Choose your favourite recipe and cook it yourself!</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your favourite recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 }
