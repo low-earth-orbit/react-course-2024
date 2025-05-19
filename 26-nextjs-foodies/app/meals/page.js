a74fcb39a413613342e1319ff628cbbb0ea3b652
Leo Hong <5917188+low-earth-orbit@users.noreply.github.com>
2025-05-19 00:34:44 -0300

    452 Suspense

--- 26-nextjs-foodies/app/meals/page.js
+++ 26-nextjs-foodies/app/meals/page.js
@@ -2,10 +2,15 @@ import MealsGrid from "@/components/meals/meals-grid";
 import classes from "./page.module.css";
 import Link from "next/link";
 import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
 
-export default async function MealsPage() {
+// outsource the fetching data to a separate component
+async function Meals() {
   const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
 
+export default function MealsPage() {
   return (
     <>
       <header className={classes.header}>
@@ -19,7 +24,11 @@ export default async function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching meals...</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
