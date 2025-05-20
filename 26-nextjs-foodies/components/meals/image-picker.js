69967c4d1b19a92dcece9a52778b32883435bbb6
Leo Hong <5917188+low-earth-orbit@users.noreply.github.com>
2025-05-19 23:03:37 -0300

    459 preview of picked image

--- 26-nextjs-foodies/components/meals/image-picker.js
+++ 26-nextjs-foodies/components/meals/image-picker.js
@@ -1,19 +1,49 @@
 "use client";
 
+import Image from "next/image";
 import classes from "./image-picker.module.css";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 export default function ImagePicker({ label, name }) {
+  const [pickedImage, setPickedImage] = useState();
   const imageInputRef = useRef();
 
   function handlePickImage() {
     imageInputRef.current.click();
   }
 
+  function handleImageChange(event) {
+    const file = event.target.files[0];
+    if (!file) {
+      setPickedImage(null);
+      return;
+    }
+
+    // covert to data URL
+    const fileReader = new FileReader();
+
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result);
+    };
+
+    fileReader.readAsDataURL(file);
+  }
+
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={classes.controls}>
+        <div className={classes.preview}>
+          {pickedImage ? (
+            <Image
+              src={pickedImage}
+              alt="The image selected by the user."
+              fill
+            />
+          ) : (
+            <p>No image is selected yet.</p>
+          )}
+        </div>
         <input
           className={classes.input}
           type="file"
@@ -21,6 +51,7 @@ export default function ImagePicker({ label, name }) {
           accept="image/png, image/jpeg"
           name={name}
           ref={imageInputRef}
+          onChange={handleImageChange}
         ></input>
         <button
           className={classes.button}
