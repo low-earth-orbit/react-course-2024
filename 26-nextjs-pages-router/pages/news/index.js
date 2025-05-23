72509046c45685a938831c20b3fd619710eedf75
Leo Hong <5917188+low-earth-orbit@users.noreply.github.com>
2025-05-23 00:28:20 -0300

    482: linking between pages

--- 26-nextjs-pages-router/pages/news/index.js
+++ 26-nextjs-pages-router/pages/news/index.js
@@ -1,3 +1,20 @@
+import Link from "next/link";
+
 export default function NewsPage() {
-  return <h1>The News Page</h1>;
+  return (
+    <>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/1">News 1</Link>
+        </li>
+        <li>
+          <Link href="/news/2">News 2</Link>
+        </li>
+        <li>
+          <Link href="/news/3">News 3</Link>
+        </li>
+      </ul>
+    </>
+  );
 }
