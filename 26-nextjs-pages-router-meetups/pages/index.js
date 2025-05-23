62b91d4bd98dbc3419eb047404f9f951b99a2da0
Leo Hong <5917188+low-earth-orbit@users.noreply.github.com>
2025-05-23 00:42:13 -0300

    486 fill in pages

--- 26-nextjs-pages-router-meetups/pages/index.js
+++ 26-nextjs-pages-router-meetups/pages/index.js
@@ -1,3 +1,28 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/4/48/NB_-_Hartland_Bridge2.jpg",
+    address: "Some address 5, 12345 Some City",
+    description: "This is a first meetup",
+  },
+  {
+    id: "m2",
+    title: "A Second Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/4/48/NB_-_Hartland_Bridge2.jpg",
+    address: "Some address 10, 12345 Some City",
+    description: "This is a second meetup",
+  },
+];
+
 export default function HomePage() {
-  return null;
+  return (
+    <>
+      <MeetupList meetups={DUMMY_MEETUPS} />
+    </>
+  );
 }
