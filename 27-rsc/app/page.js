5f543da05869e7665bd34930b68200727aa67f9a
Leo Hong <5917188+low-earth-orbit@users.noreply.github.com>
2025-05-25 20:48:05 -0300

    515: ErrorBoundary

--- 27-rsc/app/page.js
+++ 27-rsc/app/page.js
@@ -1,5 +1,6 @@
 import ClientDemo from "@/components/ClientDemo";
 import DataFetchingDemo from "@/components/DataFetchingDemo";
+import ErrorBoundary from "@/components/ErrorBoundary";
 import RSCDemo from "@/components/RSCDemo";
 import ServerActionsDemo from "@/components/ServerActionsDemo";
 import UsePromiseDemo from "@/components/UsePromisesDemo";
@@ -7,20 +8,22 @@ import fs from "node:fs/promises";
 import { Suspense } from "react";
 
 export default async function Home() {
-  const fetchUserPromise = new Promise((resolve) =>
+  const fetchUserPromise = new Promise((resolve, reject) =>
     setTimeout(async () => {
       const data = await fs.readFile("dummy-db.json", "utf-8");
       const users = JSON.parse(data);
-      resolve(users);
+      // resolve(users);
+      reject(new Error("Failed to fetch users!"));
     }, 2000)
   ); // simulate delay
 
   return (
     <main>
-      <Suspense fallback={<p>Loading...</p>}>
-        <UsePromiseDemo usersPromise={fetchUserPromise} />
-      </Suspense>
-
+      <ErrorBoundary fallback={"Something went wrong!"}>
+        <Suspense fallback={<p>Loading...</p>}>
+          <UsePromiseDemo usersPromise={fetchUserPromise} />
+        </Suspense>
+      </ErrorBoundary>
       {/* <ServerActionsDemo /> */}
       {/* <DataFetchingDemo /> */}
       {/* <ClientDemo>
