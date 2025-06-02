1d290fc0d80ecea194c4ce7892ca6a33ed9b0d45
Leo Hong <5917188+low-earth-orbit@users.noreply.github.com>
2025-06-01 21:41:49 -0300

    547

--- 29-patterns/src/App.jsx
+++ 29-patterns/src/App.jsx
@@ -1,47 +1,95 @@
 import Accordion from "./components/Accordion/Accordion";
-import AccordionItem from "./components/Accordion/AccordionItem";
+import SearchableList from "./components/SearchableList/SearchableList";
+
+import savannaImg from "./assets/african-savanna.jpg";
+import amazonImg from "./assets/amazon-river.jpg";
+import caribbeanImg from "./assets/caribbean-beach.jpg";
+import desertImg from "./assets/desert-dunes.jpg";
+import forestImg from "./assets/forest-waterfall.jpg";
+import Place from "./Place";
+
+const PLACES = [
+  {
+    id: "african-savanna",
+    image: savannaImg,
+    title: "African Savanna",
+    description: "Experience the beauty of nature.",
+  },
+  {
+    id: "amazon-river",
+    image: amazonImg,
+    title: "Amazon River",
+    description: "Get to know the largest river in the world.",
+  },
+  {
+    id: "caribbean-beach",
+    image: caribbeanImg,
+    title: "Caribbean Beach",
+    description: "Enjoy the sun and the beach.",
+  },
+  {
+    id: "desert-dunes",
+    image: desertImg,
+    title: "Desert Dunes",
+    description: "Discover the desert life.",
+  },
+  {
+    id: "forest-waterfall",
+    image: forestImg,
+    title: "Forest Waterfall",
+    description: "Listen to the sound of the water.",
+  },
+];
 
 function App() {
   return (
     <main>
       <section>
         <h2>Why work with us</h2>
+        <Accordion className="accordion">
+          <Accordion.Item id="1" className="accordion-item">
+            <Accordion.Title className="accordion-item-title">
+              We are experts
+            </Accordion.Title>
+            <Accordion.Content className="accordion-item-content">
+              <article>
+                <p>You can't go wrong with us.</p>
+                <p>Some text.</p>
+              </article>
+            </Accordion.Content>
+          </Accordion.Item>
+          <Accordion.Item id="2" className="accordion-item">
+            <Accordion.Title className="accordion-item-title">
+              We are experts
+            </Accordion.Title>
+            <Accordion.Content className="accordion-item-content">
+              <article>
+                <p>You can't go wrong with us.</p>
+                <p>Some text.</p>
+              </article>
+            </Accordion.Content>
+          </Accordion.Item>
+          <Accordion.Item id="3" className="accordion-item">
+            <Accordion.Title className="accordion-item-title">
+              We are experts
+            </Accordion.Title>
+            <Accordion.Content className="accordion-item-content">
+              <article>
+                <p>You can't go wrong with us.</p>
+                <p>Some text.</p>
+              </article>
+            </Accordion.Content>
+          </Accordion.Item>
+        </Accordion>
       </section>
-      <Accordion className="accordion">
-        <Accordion.Item id="1" className="accordion-item">
-          <Accordion.Title className="accordion-item-title">
-            We are experts
-          </Accordion.Title>
-          <Accordion.Content className="accordion-item-content">
-            <article>
-              <p>You can't go wrong with us.</p>
-              <p>Some text.</p>
-            </article>
-          </Accordion.Content>
-        </Accordion.Item>
-        <Accordion.Item id="2" className="accordion-item">
-          <Accordion.Title className="accordion-item-title">
-            We are experts
-          </Accordion.Title>
-          <Accordion.Content className="accordion-item-content">
-            <article>
-              <p>You can't go wrong with us.</p>
-              <p>Some text.</p>
-            </article>
-          </Accordion.Content>
-        </Accordion.Item>
-        <Accordion.Item id="3" className="accordion-item">
-          <Accordion.Title className="accordion-item-title">
-            We are experts
-          </Accordion.Title>
-          <Accordion.Content className="accordion-item-content">
-            <article>
-              <p>You can't go wrong with us.</p>
-              <p>Some text.</p>
-            </article>
-          </Accordion.Content>
-        </Accordion.Item>
-      </Accordion>
+      <section>
+        <SearchableList items={PLACES}>
+          {(item) => <Place item={item} />}
+        </SearchableList>
+        <SearchableList items={["Item 1", "Item 2"]}>
+          {(item) => item}
+        </SearchableList>
+      </section>
     </main>
   );
 }
