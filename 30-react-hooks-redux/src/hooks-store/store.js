404c74f1e5eca61fe43863bf968dfd6f0647c489
Leo Hong <5917188+low-earth-orbit@users.noreply.github.com>
2025-06-03 00:30:22 -0300

    557

---
+++ 30-react-hooks-redux/src/hooks-store/store.js
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+
+let globalState = {};
+let listeners = [];
+let actions = {};
+
+export const useStore = () => {
+  const setState = useState(globalState)[1];
+
+  const dispatch = (actionIdentifier) => {
+    const newState = actions[actionIdentifier](globalState);
+    globalState = { ...globalState, ...newState };
+
+    for (const listener of listeners) {
+      listener(globalState);
+    }
+  };
+
+  useEffect(() => {
+    // when the component mounts, set the state to the global state
+    listeners.push(setState);
+
+    return () => {
+      // when the component unmounts, remove the listener
+      listeners = listeners.filter((listener) => listener !== setState);
+    };
+  }, [setState]); // updating function never changes, this useEffect will only run once when the component mounts
+
+  return [globalState, dispatch];
+};
+
+export const initStore = (userActions, initialState) => {
+  if (initialState) {
+    globalState = { ...globalState, ...initialState };
+  }
+
+  actions = { ...actions, ...userActions };
+};
