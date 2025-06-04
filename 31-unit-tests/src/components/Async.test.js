4f4fa8b1f372e44f14942d38f44c2247e318216c
Leo Hong <5917188+low-earth-orbit@users.noreply.github.com>
2025-06-04 01:48:37 -0300

    576 mocks

--- 31-unit-tests/src/components/Async.test.js
+++ 31-unit-tests/src/components/Async.test.js
@@ -3,6 +3,13 @@ import Async from "./Async";
 
 describe("Async Component", () => {
   test("renders posts if request succeeds", async () => {
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [
+        { id: "1", title: "Post 1" },
+        { id: "2", title: "Post 2" },
+      ],
+    });
     render(<Async />);
     const listItemElements = await screen.findAllByRole("listitem");
     expect(listItemElements).not.toHaveLength(0);
