181584b5ff6ebe96c2bf387435877cb1b867e58a
Leo Hong <5917188+low-earth-orbit@users.noreply.github.com>
2025-06-08 19:30:10 -0300

    597

--- 32-react-ts/src/App.tsx
+++ 32-react-ts/src/App.tsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Todos from "./components/Todos";
 import Todo from "./models/todos";
+import NewTodo from "./components/NewTodo";
+
 function App() {
-  const todos = [new Todo("Learn react"), new Todo("Learn ts")];
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+    setTodos((prev) => {
+      return prev.concat(newTodo);
+    });
+  };
 
   return (
     <div>
+      <NewTodo onAddTodo={addTodoHandler} />
       <Todos items={todos} />
     </div>
   );
