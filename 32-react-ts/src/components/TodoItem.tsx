d2e93b67741093531ac32a7940d4956b7344a15d
Leo Hong <5917188+low-earth-orbit@users.noreply.github.com>
2025-06-08 19:34:18 -0300

    598

--- 32-react-ts/src/components/TodoItem.tsx
+++ 32-react-ts/src/components/TodoItem.tsx
@@ -1,7 +1,8 @@
 import Todo from "../models/todos";
+import classes from "./TodoItem.module.css";
 
 const TodoItem: React.FC<{ item: Todo }> = (props) => {
-  return <li>{props.item.text}</li>;
+  return <li className={classes.item}>{props.item.text}</li>;
 };
 
 export default TodoItem;
